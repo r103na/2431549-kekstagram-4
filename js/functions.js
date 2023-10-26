5780aeef86e2b176bbf9bd0d174a0842d2e3ce8b
rina <83953894+r103na@users.noreply.github.com>
2023-10-26 16:49:31 +0500

    Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -21,6 +21,27 @@ const getNumber = (str) => {
   return parseInt(numberString, 10);
 };
 
+const stripHours = (hour) => hour.split(':').map(Number);
+
+const checkTime = (startTime, endTime, meetingTime, meetingDuration) => {
+  let isMeetingOut = true;
+
+  const [startHour, startMinute] = stripHours(startTime);
+  const [endHour, endMinute] = stripHours(endTime);
+  const [meetingHour, meetingMinute] = stripHours(meetingTime);
+
+  const startMinutes = startHour * 60 + startMinute;
+  const endMinutes = endHour * 60 + endMinute;
+  const meetingStartMinutes = meetingHour * 60 + meetingMinute;
+
+  const meetingEndMinutes = meetingStartMinutes + meetingDuration;
+
+  isMeetingOut = meetingStartMinutes >= startMinutes && meetingEndMinutes <= endMinutes;
+
+  return isMeetingOut;
+};
+
 isStringLengthValid('проверяемая строка', 20);
 isPalindrome('роза АЗОР');
 getNumber('2023 1.5');
+checkTime();
