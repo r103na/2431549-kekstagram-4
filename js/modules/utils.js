253be65846cbdd6ca70d5a6705b7b3018d873ea3
rina <83953894+r103na@users.noreply.github.com>
2023-12-17 14:00:56 +0500

    12.12. Перламутровые пуговицы

--- js/modules/utils.js
+++ js/modules/utils.js
@@ -8,4 +8,26 @@ const getRandomInteger = (min, max) => {
 
 const getRandomElement = (array) => array[getRandomInteger(0, array.length - 1)];
 
-export { getRandomElement, getRandomInteger };
+function debounce(callback, timeoutDelay = 500) {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+function throttle(callback, delayBetweenFrames) {
+  let lastTime = 0;
+
+  return (...rest) => {
+    const now = new Date();
+
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
+
+export { getRandomElement, getRandomInteger, debounce, throttle };
